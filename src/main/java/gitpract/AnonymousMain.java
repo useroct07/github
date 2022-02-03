66f4d7a93560f65ffdffad59bfd789567c48ca6c
sid sidharth <sidharth17081998@gmail.com>
2022-02-04 00:57:32 +0530

    added first file to side branch sid

---
+++ src/main/java/gitpract/AnonymousMain.java
@@ -0,0 +1,11 @@
+package gitpract;
+import org.apache.log4j.Logger;
+
+public class AnonymousMain {
+    static Logger log=Logger.getLogger(AnonymousMain.class.getClass());
+
+    public static void main(String[] args) {
+        log.info("checking");
+    }
+
+}
