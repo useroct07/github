3287b33cb018d631e1c109434b0c2cc553579ac9
rohitnaik07 <17ec25@aitdgoa.edu.in>
2022-02-03 11:50:55 +0530

    Added first code to master branch

---
+++ src/main/java/gitpract/HelloGit.java
@@ -0,0 +1,12 @@
+package gitpract;
+
+import org.apache.log4j.Logger;
+
+public class HelloGit {
+
+    static Logger log = Logger.getLogger(HelloGit.class.getClass());
+
+    public static void main(String[] args) {
+        log.info("HelloGit");
+    }
+}
